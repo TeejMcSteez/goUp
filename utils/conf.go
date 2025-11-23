3e9aca155919b698a4ec88fb877638b7d933973a
TeejMcSteez <tjhall047@gmail.com>
2025-11-22 22:33:16 -0500

    Added configuration reader

---
+++ utils/conf.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"gopkg.in/yaml.v3"
+	"os"
+
+)
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
