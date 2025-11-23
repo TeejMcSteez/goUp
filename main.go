60036c3776f22682290c44b45a8710b33e2f454e
TeejMcSteez <tjhall047@gmail.com>
2025-11-23 00:06:25 -0500

    Added comments and final to scheduler in main.go

--- main.go
+++ main.go
@@ -19,21 +19,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	for name, svc := range cfg.Services {
 		if !slices.Contains(serviceEndpoints, svc.URL) {
 			fmt.Println("Adding ", name, "to service endpoints")
 			serviceEndpoints = append(serviceEndpoints, svc.URL)
 		}
 	}
-
+	// Get current service data before full launch
 	var svcData = utils.GetServiceData(serviceEndpoints)
+	// Create blank data store
 	var dataStore utils.SharedData = *utils.NewStore()
-
+	// Set current data to most recent fetch
+	dataStore.Set(svcData)
+	// Start's scheduler
 	scheduler.StartScheduler(serviceEndpoints, &dataStore)
-
+	// Starts http server
 	ret, err := server.Start(svcData)
-
 	if err != nil {
 		panic(err)
 	} else {
