508692190c678514629cdfa393ebdbffee3c0b6e
TeejMcSteez <tjhall047@gmail.com>
2025-11-22 23:43:38 -0500

    Added small scheduler with goroutines (very nice) and ticker's to fire GetServiceData and update the memory in main via pointer

---
+++ scheduler/scheduler.go
@@ -0,0 +1,22 @@
+package scheduler
+
+import (
+	"goUp/utils"
+	"time"
+	"fmt"
+)
+/*
+ Start's a goroutine with a ticker which every 30 seconds gets and updates service data in main
+*/
+func StartScheduler(serviceEndpoints []string, currData *[]utils.ServiceData) {
+    fmt.Println("Starting service data ticker")
+    ticker := time.NewTicker(30 * time.Second)
+
+    go func() {
+        for range ticker.C {
+            data := utils.GetServiceData(serviceEndpoints)
+            *currData = data
+            fmt.Println("Fetched data")
+        }
+    }()
+}
