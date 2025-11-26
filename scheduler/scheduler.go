97587a849c1d703a553662c17fc8258dc78ccde1
TeejMcSteez <tjhall047@gmail.com>
2025-11-25 21:45:33 -0500

    Update types in scheduler

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -8,7 +8,7 @@ import (
 /*
  Start's a goroutine with a ticker which every 30 seconds gets and updates service data in main
 */
-func StartScheduler(serviceEndpoints []string, currData *utils.SharedData) {
+func StartScheduler(serviceEndpoints []utils.Service, currData *utils.SharedData) {
     fmt.Println("Starting service data ticker")
     ticker := time.NewTicker(30 * time.Second)
 
