0b1735f0a6610d2bc0dee8e130d84c1ae194b2b6
TeejMcSteez <tjhall047@gmail.com>
2025-12-11 16:18:59 -0500

    Fix import bug and SQL statement error, also added GetRecentData function for frontend ease of display

--- utils/dataStore.go
+++ utils/dataStore.go
@@ -2,33 +2,10 @@ package utils
 
 import (
 	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 	"fmt"
 	"log"
 )
-
-// Creates a new empty data store for ServiceData
-func NewStore() *SharedData {
-	return &SharedData{
-		data: make([]ServiceData, 0),
-	}
-}
-
-// Writer: Sets new data
-func (s *SharedData) Set(data []ServiceData) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.data = data
-}
-
-// Reader: get a copy of the current slice
-func (s *SharedData) Get() []ServiceData {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-
-	out := make([]ServiceData, len(s.data))
-	copy(out, s.data)
-	return out
-}
 
 func InitDB() *sql.DB {
 	db, err := sql.Open("sqlite3", "./serviceData.db")
@@ -36,9 +13,7 @@ func InitDB() *sql.DB {
 		log.Fatal(err)
 	}
 
-	defer db.Close()
-
-	createTableSQL := `CREATE TBALE IF NOT EXISTS service_data (
+	createTableSQL := `CREATE TABLE IF NOT EXISTS service_data (
 		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
 		"service_name" TEXT,
 		"service_HTTP_response" TEXT,
@@ -71,17 +46,18 @@ func InsertData(db *sql.DB, sd ServiceData) {
 func GetData(db *sql.DB) []ServiceData {
 	sd := []ServiceData{}
 
-	row, err := db.Query("SELECT id, service_name, service_HTTP_response, service_API_response from service_data")
+	row, err := db.Query("SELECT id, service_name, service_HTTP_response, service_API_response FROM service_data;")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer row.Close()
 
 	for row.Next() {
+		var id int
 		var svc_name string
 		var svc_http_res string
 		var svc_api_res string
-		err = row.Scan(&svc_name, &svc_http_res, &svc_api_res)
+		err = row.Scan(&id, &svc_name, &svc_http_res, &svc_api_res)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -89,4 +65,31 @@ func GetData(db *sql.DB) []ServiceData {
 	}
 
 	return sd
+}
+
+func GetRecentData(db *sql.DB) []ServiceData {
+	numOfServices := len(GetServiceEndpoints())
+	sd := []ServiceData{}
+
+	statement := fmt.Sprintf("SELECT * FROM service_data ORDER BY id DESC LIMIT %v;", numOfServices)
+
+	row, err := db.Query(statement)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+
+	for row.Next() {
+		var id int
+		var svc_name string
+		var svc_http_res string
+		var svc_api_res string
+		err = row.Scan(&id, &svc_name, &svc_http_res, &svc_api_res)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sd = append(sd, ServiceData{ServiceName: svc_name, ServiceHTTPResponse: svc_http_res, ServiceAPIResponse: svc_api_res})
+	}
+	return sd
+
 }
