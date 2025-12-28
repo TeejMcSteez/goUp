ac9cbb22aa5c16f5846f29d2f6472b4a5b0f7038
TeejMcSteez <teejmcsteez@gmail.com>
2025-12-28 16:13:19 -0500

    Fix test error on new Check method where no config throws an error

--- tests/serviceChecker_test.go
+++ tests/serviceChecker_test.go
@@ -3,17 +3,24 @@ package utils_test
 import (
 	"goUp/utils"
 	"testing"
+	"log"
 )
 
 func TestServiceCheck(t *testing.T) {
+	cfg, err := utils.LoadConfig("../services.yml")
+	if err != nil {
+		log.Printf("Error occured loading config: %v", err)
+		t.Fail()
+	}
 	sd := []utils.ServiceData{
 		{ServiceName: "test1", ServiceHTTPResponse: "200", ServiceAPIResponse: "", ServiceResponseTime: ""},
 		{ServiceName: "test2", ServiceHTTPResponse: "303", ServiceAPIResponse: "", ServiceResponseTime: ""},
 	}
-
+	utils.Current_Config = cfg
 	res, err := utils.Check(sd)
 
 	if err != nil {
+		log.Printf("%v", err)
 		t.Error("Error checking data")
 	}
 
