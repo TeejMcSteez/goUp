459f67bb4eb5b87f318884dfbce9b92efb528900
TeejMcSteez <tjhall047@gmail.com>
2026-01-14 17:28:48 -0500

    go fmt & add test for dbstat for testing

---
+++ tests/db_stat_test.go
@@ -0,0 +1,41 @@
+package utils_test
+
+import (
+	"goUp/utils"
+	"log"
+	"testing"
+)
+
+// TODO: Cleanup db files after test
+func TestGetStat(t *testing.T) {
+	ymlContent := `db_path: "./test_data.db"
+services:
+  service2:
+    url: "https://example.com"
+    retry: 2
+  service4:
+    url: "https://www.google.com"
+    retry: 1
+`
+	cleanup := createTestYML(ymlContent, t)
+	defer cleanup()
+
+	if err := utils.Setup(); err != nil {
+		log.Printf("Error occured during setup: %v", err)
+		t.Fail()
+	}
+
+	db, err := utils.InitDB()
+	if err != nil {
+		log.Printf("Error occured during DB init: %v", err)
+		t.Fail()
+	}
+
+	data, err := utils.GetDbStat(db)
+	if err != nil {
+		log.Printf("Error occured getting database statistics: %v", err)
+		t.Fail()
+	}
+	log.Printf("DB Stats: %v", data)
+
+}
