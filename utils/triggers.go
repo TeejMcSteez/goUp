a88296a2cb8c73c69f5a4f9301c19d2b189f59e1
TeejMcSteez <teejmcsteez@gmail.com>
2026-02-12 21:09:52 -0500

    Remove URL if checks around strategy fires as it will not be called if config does not exist

--- utils/triggers.go
+++ utils/triggers.go
@@ -73,40 +73,37 @@ func (t *Trigger) FireMqtt(data []ServiceData) {
 		log.Println("Connection Lost: " + err.Error())
 	}
 
-	if t.MQTT.Mqtt_broker != nil {
-		opts := mqtt.NewClientOptions()
-		opts.AddBroker(*t.MQTT.Mqtt_broker)
-		opts.SetClientID("goUp MQTT")
-		opts.OnConnect = connectHandler
-		opts.OnConnectionLost = lostHandler
-		if t.MQTT.Mqtt_username != nil || t.MQTT.Mqtt_key != nil {
-			opts.SetUsername(*t.MQTT.Mqtt_username)
-			opts.SetPassword(*t.MQTT.Mqtt_key)
-		}
-
-		client := mqtt.NewClient(opts)
-
-		if token := client.Connect(); token.Wait() && token.Error() != nil {
-			log.Println("Error pushing to MQTT client: " + token.Error().Error())
-		}
-
-		if jsonData, err := json.Marshal(data); err != nil {
-			log.Printf("Error formatting service data into JSON: %v\n", err)
-		} else {
-			// Keep retain to true to store last known good message
-			token := client.Publish("goup_status", 0, true, jsonData)
-			token.Done()
-
-			if err := token.Error(); err != nil {
-				log.Printf("Error with MQTT token: %v\n", err)
-			}
-			log.Println("Disconnecting from MQTT broker, sent message complete")
-		}
-
-		client.Disconnect(500)
-	} else {
-		log.Println("No MQTT broker setup")
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(*t.MQTT.Mqtt_broker)
+	opts.SetClientID("goUp MQTT")
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = lostHandler
+	if t.MQTT.Mqtt_username != nil || t.MQTT.Mqtt_key != nil {
+		opts.SetUsername(*t.MQTT.Mqtt_username)
+		opts.SetPassword(*t.MQTT.Mqtt_key)
 	}
+
+	client := mqtt.NewClient(opts)
+
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Println("Error pushing to MQTT client: " + token.Error().Error())
+	}
+
+	if jsonData, err := json.Marshal(data); err != nil {
+		log.Printf("Error formatting service data into JSON: %v\n", err)
+	} else {
+		// Keep retain to true to store last known good message
+		token := client.Publish("goup_status", 0, true, jsonData)
+		token.Done()
+
+		if err := token.Error(); err != nil {
+			log.Printf("Error with MQTT token: %v\n", err)
+		}
+		log.Println("Disconnecting from MQTT broker, sent message complete")
+	}
+
+	client.Disconnect(500)
+	
 }
 
 // Takes in trigger message and checks config for any special message paramters
@@ -134,36 +131,32 @@ func (t *Trigger) getWebhookMessage(data []ServiceData) (io.Reader, error) {
 }
 
 func (t *Trigger) FireWebhook(data []ServiceData) {
-	if t.Webhook.Webhook_url != nil {
-		jsonMessage, err := t.getWebhookMessage(data)
-		if err != nil {
-			log.Printf("Failed parsing json service data message: %v\n", err)
-		}
-		log.Println("Firing webhook")
-
-		req, err := http.NewRequest("POST", *t.Webhook.Webhook_url, jsonMessage)
-		if err != nil {
-			log.Printf("Error creating webhook request: %v\n", err)
-			return
-		}
-		req.Header.Add("Content-Type", "application/json")
-		if t.Webhook.Webhook_key_string != nil {
-			req.Header.Add("Authorization", *t.Webhook.Webhook_key_string)
-		}
-		res, err := http.DefaultClient.Do(req)
-		if err != nil {
-			log.Printf("Error sending webhook: %v\n", err)
-			return
-		}
-		defer func() {
-			if err := res.Body.Close(); err != nil {
-				log.Printf("Error closing webhook request: %v", err)
-			}
-		}()
-
-		log.Printf("Webhook sent, status: %s\n", res.Status)
-
-	} else {
-		log.Println("No webhook setup")
+	jsonMessage, err := t.getWebhookMessage(data)
+	if err != nil {
+		log.Printf("Failed parsing json service data message: %v\n", err)
 	}
+	log.Println("Firing webhook")
+
+	req, err := http.NewRequest("POST", *t.Webhook.Webhook_url, jsonMessage)
+	if err != nil {
+		log.Printf("Error creating webhook request: %v\n", err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	if t.Webhook.Webhook_key_string != nil {
+		req.Header.Add("Authorization", *t.Webhook.Webhook_key_string)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("Error sending webhook: %v\n", err)
+		return
+	}
+	defer func() {
+		if err := res.Body.Close(); err != nil {
+			log.Printf("Error closing webhook request: %v", err)
+		}
+	}()
+
+	log.Printf("Webhook sent, status: %s\n", res.Status)
+
 }
