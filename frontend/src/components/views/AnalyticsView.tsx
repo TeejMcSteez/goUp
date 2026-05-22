c06beba8d375460344d4a45865d704831f4aa2cb
TeejMcSteez <teejmcsteez@gmail.com>
2026-05-21 20:40:27 -0400

    link uptime chart into Analytics view and re-order view arrangement

--- frontend/src/components/views/AnalyticsView.tsx
+++ frontend/src/components/views/AnalyticsView.tsx
@@ -1,15 +1,19 @@
 import Island from "../layout/Island";
 import UptimeChart from "../UptimeChart";
 import ErrorLogViewer from "../ErrorLogViewer";
+import ResponseTimeChart from "../ResponseTimeChart";
 
 export default function AnalyticsView() {
   return (
-    <div className="analytics-view">
+    <div className="w-full max-w-full overflow-x-hidden">
+      <Island title="Error Log">
+        <ErrorLogViewer />
+      </Island>
       <Island title="Failure Graph">
         <UptimeChart />
       </Island>
-      <Island title="Error Log">
-        <ErrorLogViewer />
+      <Island title="Response Times">
+        <ResponseTimeChart />
       </Island>
     </div>
   );
