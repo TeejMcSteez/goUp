5e0cfcef1dda474a81d84d268e81475135f670c1
TeejMcSteez <teejmcsteez@gmail.com>
2026-06-21 01:48:04 -0400

    wire API routes to handler

--- server/server.go
+++ server/server.go
@@ -54,6 +54,8 @@ func (s *Server) Start() error {
 	http.HandleFunc("/api/config/webhook", s.ConfigWebhookApi)
 	http.HandleFunc("/api/config/smtp", s.ConfigSMTPApi)
 	http.HandleFunc("/api/config/gotify", s.ConfigGotifyApi)
+	http.HandleFunc("/api/config/slack", s.ConfigSlackApi)
+	http.HandleFunc("/api/config/telegram", s.ConfigTelegramApi)
 	http.HandleFunc("/api/config/size", s.ConfigDatabaseApi)
 	http.HandleFunc("/swagger/", httpSwagger.WrapHandler)
 	log.Println("Starting server at http://localhost:8101/ . . .")
