423454fef1f82f26a17a03de7c52908eabb3a8ca
TeejMcSteez <teejmcsteez@gmail.com>
2026-09-02 20:50:38 -0400

    refactor: use http handler map to remove double mapping problem and remove individual function registration

--- server/server.go
+++ server/server.go
@@ -59,33 +59,41 @@ func (s *Server) Start() error {
 	} else {
 		http.HandleFunc("/", s.HandleNoUi)
 	}
-	http.HandleFunc("/health", s.Health)
-	http.HandleFunc("/api", s.Api)
-	http.HandleFunc("/api/fire", s.ManualFire)
-	http.HandleFunc("/api/schedule", s.ScheduleApi)
-	http.HandleFunc("/api/status", s.StatusApi)
-	http.HandleFunc("/api/uptime", s.UptimeAPI)
-	http.HandleFunc("/api/rt", s.GetResponseTimes)
-	http.HandleFunc("/api/tls", s.GetTls)
-	http.HandleFunc("/api/errors", s.GetErrorData)
-	http.HandleFunc("/api/db/size", s.GetDatabaseSize)
-	http.HandleFunc("/api/db/persist", s.GetDatabasePersistence)
-	http.HandleFunc("/api/db/clear", s.ClearDatabase)
-	http.HandleFunc("/api/config", s.ReadConfigData)
-	http.HandleFunc("/api/config/service", s.ConfigServiceApi)
-	http.HandleFunc("/api/config/service/active", s.ConfigActiveApi)
-	http.HandleFunc("/api/config/mqtt", s.ConfigMQTTApi)
-	http.HandleFunc("/api/config/webhook", s.ConfigWebhookApi)
-	http.HandleFunc("/api/config/smtp", s.ConfigSMTPApi)
-	http.HandleFunc("/api/config/gotify", s.ConfigGotifyApi)
-	http.HandleFunc("/api/config/slack", s.ConfigSlackApi)
-	http.HandleFunc("/api/config/telegram", s.ConfigTelegramApi)
-	http.HandleFunc("/api/config/ha", s.ConfigHAApi)
-	http.HandleFunc("/api/config/discord", s.ConfigDiscordApi)
-	http.HandleFunc("/api/config/backoff", s.ConfigBackoffApi)
-	http.HandleFunc("/api/config/size", s.ConfigDatabaseApi)
-	http.HandleFunc("/swagger/", httpSwagger.WrapHandler)
-	http.HandleFunc("/ws", s.handleWs)
+
+	handlers := map[string]func(w http.ResponseWriter, req *http.Request){
+		"/health":                    s.Health,
+		"/api":                       s.Api,
+		"/api/fire":                  s.ManualFire,
+		"/api/schedule":              s.ScheduleApi,
+		"/api/status":                s.StatusApi,
+		"/api/uptime":                s.UptimeAPI,
+		"/api/rt":                    s.GetResponseTimes,
+		"/api/tls":                   s.GetTls,
+		"/api/errors":                s.GetErrorData,
+		"/api/db/size":               s.GetDatabaseSize,
+		"/api/db/persist":            s.GetDatabasePersistence,
+		"/api/db/clear":              s.ClearDatabase,
+		"/api/config":                s.ReadConfigData,
+		"/api/config/service":        s.ConfigServiceApi,
+		"/api/config/service/active": s.ConfigActiveApi,
+		"/api/config/mqtt":           s.ConfigMQTTApi,
+		"/api/config/webhook":        s.ConfigWebhookApi,
+		"/api/config/smtp":           s.ConfigSMTPApi,
+		"/api/config/gotify":         s.ConfigGotifyApi,
+		"/api/config/slack":          s.ConfigSlackApi,
+		"/api/config/telegram":       s.ConfigTelegramApi,
+		"/api/config/ha":             s.ConfigHAApi,
+		"/api/config/discord":        s.ConfigDiscordApi,
+		"/api/config/backoff":        s.ConfigBackoffApi,
+		"/api/config/size":           s.ConfigDatabaseApi,
+		"/swagger/":                  httpSwagger.WrapHandler,
+		"/ws":                        s.handleWs,
+	}
+
+	for url, handler := range handlers {
+		http.HandleFunc(url, handler)
+	}
+
 	s.handler.next = http.DefaultServeMux
 	slog.Info("Starting server", "address", "http://localhost:8101/")
 	if err := http.ListenAndServe(":8101", s.handler); err != nil {
